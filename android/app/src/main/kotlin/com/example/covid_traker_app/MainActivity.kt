d1450926a2357278c50e5c3b7fae152e972f3608
aankit09 <aankitsanvedi@gmail.com>
2022-09-19 11:46:42 +0530

    animation only in Splash Screen

---
+++ android/app/src/main/kotlin/com/example/covid_traker_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.covid_traker_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/View/splash_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller =
+      AnimationController(duration: const Duration(seconds: 3), vsync: this)
+        ..repeat();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            AnimatedBuilder(
+              animation: _controller,
+              child: Container(
+                height: 200,
+                width: MediaQuery.of(context).size.width,
+                child: const Center(
+                  child: Image(
+                    image: AssetImage('images/virus.png'),
+                  ),
+                ),
+              ),
+              builder: (BuildContext context, Widget? child) {
+                return Transform.rotate(
+                  angle: _controller.value * 2.0 * math.pi,
+                  child: child,
+                );
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
